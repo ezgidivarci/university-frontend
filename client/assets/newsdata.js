0c649f2a33faed0d140001b2ccb7eac62d34c37c
Ezgi Dıvarcı <ezgidivarci1@gmail.com>
2022-04-28 00:33:37 +0300

    first commit

---
+++ client/assets/newsdata.js
@@ -0,0 +1,24 @@
+export const newsCardSections = [
+
+    {
+        id: 1,
+        header: "haber1",
+        productDescription: "haber1 lorem1msdnmfçmö.",
+        image: "a.png"
+    },
+    {
+        id: 2,
+        header: "haber2",
+        productDescription: "haber2 lorem jdsfkhjk",
+        image: "b.png",
+
+    },
+    {
+        id: 3,
+        header: "haber3",
+        productDescription: "haber3 lorem djshflşkl",
+        image: "c.png"
+    },
+
+
+]
---
+++ client/components/News.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <section id="features" class="section-area">
+        <div class="features-top">
+            <h2 class="section-header">FEATURED SERVICE</h2>
+            <p>Vintage Cars Blog is a hub of classic car knowledge, featuring reviews.</p>
+
+        </div>
+        
+        <div class="features-body">
+            <div class="container">
+                <div class="col3">
+                    <div class="item">
+                        <div class="zoom">
+                            <!--<img :src="require(`@/img/${card.image$|||`)"> -->
+                        </div>
+                        <div class="item-text">
+                             <h1>{{cardsSection.header}}</h1>
+                            <p> {{cardsSection.productDescription}}
+                                Has there ever been a more iconic American car than the ’57 Chevy Bel Air? Of course there hasn’t, it’s not even close. Not even the Model T is as much a 
+                                symbol of its age as the Bel Air. It represents American middle class postwar prosperity perfectly, and is a rare example of a car with styling that was exactly in line with contemporary design.</p>
+                            <a href="#" class="btn-details">More Details</a>
+
+                        </div>
+
+                    </div>
+
+                 </div>
+            </div>
+        </div>
+    </section>
+       
+
+        <div><NewsCard v-for="card in cardsSection.cards"
+        :key="card.id"
+        :card="card" /> </div>
+
+    </div>
+    
+</template>
+
+
+
+<script>
+
+export default {
+   props: ['cardsSection']
+}
+</script>
+
+<style scoped>
+
+</style>
